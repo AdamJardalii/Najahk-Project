58e97b9422ff2618789084787e9e7cd8ab9d2364
AdamJardalii <adam.jardali@lau.edu>
2025-12-08 17:30:15 +0200

    remove

--- quoteImageApp/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\PairSumController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-   Route::post('/pair-sum', [PairSumController::class, 'solve']);
